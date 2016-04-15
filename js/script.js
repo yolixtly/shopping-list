79e8869b6042da7fd4a6b948f921817201d4afa5
yolixtly <yolixtly_13@hotmail.com>
2016-04-15 10:21:02 -0400

    cleaner code

--- js/script.js
+++ js/script.js
@@ -1,55 +1,51 @@
-	$(document).ready(function(){
+  $(document).ready(function() {
 
-	/*-----Global Variables -----*/
+  /*-----Global Variables -----*/
 
-	var checkMark = "<span class= 'itemNotChecked'></span>"; 
-	var crossMark = "<span class= 'deleteButton'></span>";
-
-
-	/*-----Function to submit the items of input with Add Me button -----*/
-
-	function addProduct(){
-		var itemInput = $(".productInput").val(); 	//items logged inside the text box "input"
-		var itemOnList =  "<li class ='newItem'>" + crossMark + itemInput + checkMark + "</li>";
-		$(".added-items").prepend(itemOnList);	
-		$(".productInput").val(""); 				//clean input box after clicking to add the last product
-
-	};
-
-	/*Calling the addProduct function with Click event*/
-
-		$(".add-button").click(addProduct);
+  var checkMark = "<span class= 'itemNotChecked'></span>"; 
+  var crossMark = "<span class= 'deleteButton'></span>";
 
 
-	/*---Function to check completed items---*/
+  /*-----Function to submit the items of input with Add Me button -----*/
 
-	$(".added-items").on('click', '.itemNotChecked, .itemChecked', 
-		function(){
+  function addProduct() {
+    var itemInput = $(".productInput").val();   //items logged inside the text box "input"
+    var itemOnList =  "<li class ='newItem'>" + crossMark + itemInput + checkMark + "</li>";
+    $(".added-items").prepend(itemOnList);  
+    $(".productInput").val("");         //clean input box after clicking to add the last product
 
-			$(this).closest('li').toggleClass('newItem');
-			$(this).closest('li').toggleClass("itemComplete");
+  };
+
+  /*Calling the addProduct function with Click event*/
+
+    $(".add-button").click(addProduct);
 
 
-		    $(this).toggleClass("itemNotChecked");
-		    $(this).toggleClass("itemChecked");
-				});
+  /*---Function to check completed items---*/
+
+  $(".added-items").on('click', '.itemNotChecked, .itemChecked', 
+    function() {
+      $(this).closest('li').toggleClass('newItem');
+      $(this).closest('li').toggleClass("itemComplete");
+      $(this).toggleClass("itemNotChecked");
+      $(this).toggleClass("itemChecked");
+    });
 
 
-	/*---function to delete items---*/
+  /*---function to delete items---*/
 
-	$(document).on("click", '.deleteButton', 
-		function(){
-			$(this).closest('li').fadeOut(300);
+  $(document).on("click", '.deleteButton', 
+    function() {
+      $(this).closest('li').fadeOut(300);
 
-	})
-
+  });
 });
 
-	
+  
 
 
 
 
-	
+  
 
 
